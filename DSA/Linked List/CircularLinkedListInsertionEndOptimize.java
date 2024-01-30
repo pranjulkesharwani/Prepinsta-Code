f9b1bf6b3a4f690aaba3353dd091ab0256916eca
pranjul kesharwani <pranjulkesharwani08@gmail.com>
2024-01-30 11:15:29 +0530

    circular Linked list Insertion End Optimize

---
+++ DSA/Linked List/CircularLinkedListInsertionEndOptimize.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class CircularLinkedListInsertionEndOptimize {
+    static Node insertLast(Node head, int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            newNode.next = newNode;
+            return newNode;
+        }
+        newNode.next = head.next;
+        head.next = newNode;
+        int temp = head.data;
+        head.data = newNode.data;
+        newNode.data = temp;
+        return newNode;
+    }
+
+    static void display(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node temp = head;
+        do {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        } while (temp != head);
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+
+        head = insertLast(head, 12);
+        head = insertLast(head, 16);
+        head = insertLast(head, 20);
+        head = insertLast(head, 24);
+        head = insertLast(head, 30);
+        head = insertLast(head, 22);
+
+        System.out.println(head.data);
+        display(head);
+    }
+
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
