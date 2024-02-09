c6bd1d819b6b51efd1962aa0d11f7037207466f8
pranjul kesharwani <pranjulkesharwani08@gmail.com>
2024-02-09 10:30:18 +0530

    Level Order Traversal 2

---
+++ DSA/Tree/LevelOrderTravesal2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+
+class Tree {
+    Node root;
+
+    void printLevelOrder(Node root) {
+        if (root == null)
+            return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            System.out.print(node.data + " ");
+            if (node.left != null) {
+                q.add(node.left);
+            }
+            if (node.right != null) {
+                q.add(node.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+        int k = 2;
+
+        tree.printLevelOrder(tree.root);
+    }
+}
